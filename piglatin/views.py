15e455e67b57ece77dae3e46d69b8cf4386abedb
Angela Maugey <a.maugey@gmail.com>
2017-08-25 18:31:15 +0100

    getting things form data from page to another

--- piglatin/views.py
+++ piglatin/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def hello(request):
+def home(request):
     return render(request, 'home.html')
+
+def translate(request):
+    request.GET["originaltext"]
+    return HttpResponse("You're on the translate page!" + request.GET["originaltext"])
