9bf41bbcb233b37729adf979fe662adb11d4ef65
Angela Maugey <a.maugey@gmail.com>
2017-08-27 17:22:08 +0100

    adding an about page. Final commit

--- piglatin/views.py
+++ piglatin/views.py
@@ -4,6 +4,9 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'home.html')
 
+def about(request):
+    return render(request, 'about.html')
+
 def translate(request):
 
     original = request.GET["originaltext"].lower()
